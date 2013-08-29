c5b7db4d5265b6f2d142c50b5f423cbdb01906ca
xuyuanme <xuyuanme@gmail.com>
2013-08-29 20:08:13 +0800

    Add js Message model

---
+++ src/main/webapp/app/models/message.js
@@ -0,0 +1,18 @@
+/**
+ * Define Require module with dependencies
+ */
+define([
+  'underscore',
+  'backbone'
+], function (_, Backbone) {
+  /**
+   * Message Model
+   */
+  var Message = Backbone.Model.extend({
+    // Url binding of the REST service
+    url:'data-rest/message'
+  });
+
+  // Return the view as the Require module
+  return new Message();
+});
